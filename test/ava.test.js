d9197ebfe28ae4892e2938b3b17b57fd61c871b5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-21 10:20:02 -0500

    chore(ava): prepare for Ava test for #7

---
+++ test/ava.test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import test from 'ava'
+import schemaShot from '..'
+import generate from 'fake-todos'
+
+const generateP = () => {
+  return Promise.resolve(generate(1)[0])
+}
+
+test('returns a todo', t => {
+  const todos = generate(1)
+  schemaShot(todos[0])
+})
+
+test('todo promise', async t => {
+  const todo = await generateP()
+  schemaShot(todo)
+})
